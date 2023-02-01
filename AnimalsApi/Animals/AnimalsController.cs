e979eb46d870756cf5ce1c18582c9f618193a374
Ksawery Oleszynski <ksawery.oleszynski@gmail.com>
2023-02-01 21:35:17 +0100

    init

---
+++ AnimalsApi/Animals/AnimalsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalsApi.Animals;
+
+[ApiController]
+[Route("api/animals")]
+public class AnimalsController : ControllerBase
+{
+   private readonly IAnimalsRepository _animalsRepository;
+
+   public AnimalsController(IAnimalsRepository repository)
+   {
+      _animalsRepository = repository;
+   }
+   [HttpGet]
+   public IActionResult GetAnimals()
+   {
+      var animals = _animalsRepository.GetAnimals();
+      return Ok(animals);
+   }
+}
---
+++ AnimalsApi/Animals/AnimalsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AnimalsApi.Animals;
+
+public interface IAnimalsRepository
+{
+    IEnumerable<Animal> GetAnimals();
+}
+
+public class Animal
+{
+    public int IdAnimal { get; init; }
+    public string Name { get; init;  }
+    public string Description { get; init; }
+    public string Category { get; init; }
+    public string Area { get; init; }
+}
+
+public class AnimalsRepository : IAnimalsRepository
+{
+    private const string ConString = "Server=localhost\\SQLEXPRESS01;Database=master;Trusted_Connection=True;TrustServerCertificate=True;";
+    public IEnumerable<Animal> GetAnimals()
+    {
+        var res = new List<Animal>();
+        using var con = new SqlConnection(ConString);
+        var command = new SqlCommand();
+        command.Connection = con;
+        command.CommandText = "SELECT * FROM Animal";
+        con.Open();
+        var dr = command.ExecuteReader();
+        while (dr.Read())
+        {
+            res.Add(new Animal
+            {
+                IdAnimal = dr.GetInt32(0),
+                Name = dr.GetString(1),
+                Description = dr.GetString(2),
+                Category = dr.GetString(3),
+                Area = dr.GetString(4),
+            });
+        }
+
+        return res;
+    }
+}
